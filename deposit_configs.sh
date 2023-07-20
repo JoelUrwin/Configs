2b69d60c28a63e2b9db116fa83998ef2f0243075
JoelUrwin <joel@joelurw.in>
2023-07-20 11:15:25 +0100

    Added scripts to update and deposit configs into their system folders.

---
+++ deposit_configs.sh
@@ -0,0 +1,5 @@
+sudo cp ./polybar/config.ini /etc/polybar/config.ini
+sudo cp ./i3/config /home/$USER/.config/i3/config
+sudo cp ./vim/.vimrc /home/$USER/.vimrc
+
+echo Put config files in the correct place!
---
+++ update-configs.sh
@@ -0,0 +1,3 @@
+sudo cp /home/$USER/.config/i3/config /home/$USER/Documents/Configs/i3/config
+sudo cp /home/$USER/.vimrc /home/$USER/Documents/Configs/vim/.vimrc
+sudo cp /etc/polybar/config.ini /home/$USER/Documents/Configs/polybar/config.ini
